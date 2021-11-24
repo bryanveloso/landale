41ec479384a92a8bbbc1e711cb99a33894ce7f92
Bryan Veloso <bryan@velo.so>
2021-11-24 14:42:14 -0800

    Normalize things.

--- renderer/stitches.config.ts
+++ renderer/stitches.config.ts
@@ -22,11 +22,25 @@ export const {
   }
 })
 
-export const normalize: Record<string, any>[] = []
-
 export type CSS = Stitches.CSS<typeof config>
 
-export const globalStyles = globalCss({
+export const normalize: Record<string, CSS>[] = [
+  {
+    ':where(*, *::before, *::after)': { boxSizing: 'border-box' },
+    ':where(*)': { margin: 0 },
+    ':where(html, body)': { height: '100%' },
+    ':where(body)': { lineHeight: 1.5 },
+    ':where(img, picture, video, canvas, svg)': {
+      display: 'block',
+      maxWidth: '100%'
+    },
+    ':where(input, button, textarea, select)': { font: 'inherit' },
+    ':where(p, h1, h2, h3, h4, h5, h6)': { overflowWrap: 'break-word' },
+    ':where(#root, #__next)': { isolation: 'isolate' }
+  }
+]
+
+export const globalStyles = globalCss(...normalize, {
   body: {
     fontFamily: 'system-ui',
     margin: 0
