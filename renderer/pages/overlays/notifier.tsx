a2f7d7fd7933dd263bfc6c747d1f1d4db22caafa
Bryan Veloso <bryan@velo.so>
2022-09-13 23:28:43 -0700

    Switch back to socket.io.

--- renderer/pages/overlays/notifier.tsx
+++ renderer/pages/overlays/notifier.tsx
@@ -1,38 +1,43 @@
-import WebSocket from 'isomorphic-ws'
 import { useEffect, useRef, useState } from 'react'
+import { io, Socket } from 'socket.io-client'
 
 import { Screen } from '@landale/components/screen'
 import { getLayout } from '@landale/layouts/for-overlay'
 
 export default function Notifier() {
-  const ws = useRef(null)
+  const ws = useRef<Socket>(null)
   const [isConnected, setIsConnected] = useState(false)
 
   useEffect(() => {
-    ws.current = new WebSocket('ws://localhost:3000')
+    ws.current = io('ws://localhost:3000')
 
-    ws.current.onopen = () => {
-      console.log(`connected`)
+    ws.current.on('connect', () => {
+      console.log(`[rykros-websocket] Connected`)
       setIsConnected(true)
-    }
+    })
 
-    ws.current.onclose = () => {
-      console.log(`disconnected`)
+    ws.current.on('disconnect', () => {
+      console.log(`[rykros-websocket] Disconnected`)
       setIsConnected(false)
-    }
+    })
 
     return () => {
-      ws.current.close()
+      ws.current.off('connect')
+      ws.current.off('disconnect')
     }
   }, [])
 
   useEffect(() => {
     if (!ws.current) return
 
-    ws.current.onmessage = (e: any) => {
+    ws.current.on('notification', (e: any) => {
       console.log(e)
+    })
+
+    return () => {
+      ws.current.off('notification')
     }
-  })
+  }, [])
 
   return (
     <div>
