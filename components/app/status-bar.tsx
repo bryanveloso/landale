c605563a4dc08a15bf3174d02abf062b74bb86e9
Bryan Veloso <bryan@velo.so>
2022-09-23 21:26:22 -0700

    Cya stitches.

--- components/app/status-bar.tsx
+++ components/app/status-bar.tsx
@@ -1,40 +1,7 @@
-import { useObs } from '@landale/hooks'
-
-import { Box } from '../box'
-import { styled } from '../../stitches.config'
+import { useObs } from '~/landale/hooks'
 
 export const StatusBar = () => {
   const { connected } = useObs()
 
-  return (
-    <Box
-      css={{
-        alignItems: 'center',
-        display: 'flex',
-        padding: '10px 0px 10px',
-        width: '100%',
-
-        fontSize: 12,
-        fontWeight: 'bold'
-      }}
-    >
-      <Status connected={connected} /> OBS Studio
-    </Box>
-  )
+  return <div connected={connected}> OBS Studio</div>
 }
-
-const Status = styled('div', {
-  backgroundColor: '$red11',
-  width: 8,
-  height: 8,
-  marginRight: 6,
-  borderRadius: '50%',
-
-  variants: {
-    connected: {
-      true: {
-        backgroundColor: '$green11'
-      }
-    }
-  }
-})
