b90c7c700a418caac0683b9055b62cdb89675830
Bryan Veloso <bryan@velo.so>
2022-10-16 23:29:45 -0700

    Cleanup?!

--- components/overlays/windows/titlebar.tsx
+++ components/overlays/windows/titlebar.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, LayoutGroup, motion } from 'framer-motion'
 import { FC, PropsWithChildren } from 'react'
 import Icon from '~/components/icons'
 
@@ -8,15 +8,17 @@ export const TitleBar: FC<PropsWithChildren> = ({ children }) => {
       <div className="flex items-center gap-3 px-5 justify-end">
         <Icon icon="chromecast-line" size={24} className="text-white/50" />
       </div>
-      <motion.div
-        layout
-        className="flex items-center justify-center gap-3 px-5 shadow-titlebar-inset bg-titlebar rounded-tr-lg overflow-hidden"
-      >
-        <Icon icon="chevron-left-line" size={24} className="text-white/50" />
-        <Icon icon="chevron-right-line" size={24} className="text-white/50" />
-        {children}
-        <Icon icon="menu-line" size={24} className="text-white/50" />
-      </motion.div>
+      <LayoutGroup>
+        <motion.div
+          layout
+          className="flex items-center justify-center gap-3 px-3 shadow-titlebar-inset bg-titlebar rounded-tr-lg overflow-hidden"
+        >
+          <Icon icon="chevron-left-line" size={24} className="text-white/50" />
+          <Icon icon="chevron-right-line" size={24} className="text-white/50" />
+          {children}
+          <Icon icon="menu-line" size={24} className="text-white/50" />
+        </motion.div>
+      </LayoutGroup>
     </div>
   )
 }
