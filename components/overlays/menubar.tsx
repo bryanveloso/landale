1c543b3eccaaecb755ae692db61d62037c38bbac
Bryan Veloso <bryan@velo.so>
2022-10-16 21:59:12 -0700

    Icons!

--- components/overlays/menubar.tsx
+++ components/overlays/menubar.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import Clock from 'react-live-clock'
 import Image from 'next/future/image'
 
-import { Battery100Icon } from '~/components/icons'
+import Icon from '~/components/icons'
 import { useHasMounted } from '~/hooks'
 
 import logo from '../../public/avalonstar.png'
@@ -17,8 +17,12 @@ export const MenuBar: FC = () => {
         <div className="flex-auto font-bold text-white">Avalonstar</div>
       </div>
       <div className="flex-1 text-gray-100 text-right tabular-nums">
-        <div className="flex justify-end gap-4">
-          <Battery100Icon className="w-6 h-6 text-green-400" />
+        <div className="flex items-center justify-end gap-4">
+          {/* <div className="flex items-center">
+            <Icon icon="clock-line" size={24} />
+            <span className="pl-2">04:00:00</span>
+          </div> */}
+          <Icon icon="battery-full-line" size={24} className="text-green-400" />
           {hasMounted && <Clock ticking format="MMM D h:mm A" />}
         </div>
       </div>
