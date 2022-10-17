197c34619fdda1b4a862959511e13631a359f6ed
Bryan Veloso <bryan@velo.so>
2022-10-16 21:59:08 -0700

    Whoops.

--- pages/overlays/shared/background.tsx
+++ pages/overlays/shared/background.tsx
@@ -16,7 +16,7 @@ import {
   RainwaveResponse
 } from '~/components/overlays/windows/rainwave'
 import { Metadata } from '~/components/overlays/windows/titlebar-metadata'
-import { useChannel, useTwitchEvent } from '~/hooks'
+import { useChannel, useHasMounted, useTwitchEvent } from '~/hooks'
 import {
   TwitchChannelUpdateEvent,
   TwitchEvent,
@@ -36,6 +36,7 @@ const Background = ({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const channel = useChannel()
   const [timestamp, setTimestamp] = useState('')
+  const hasMounted = useHasMounted()
 
   useTwitchEvent((twitchEvent: TwitchEvent) => {
     const key = hash(twitchEvent)
@@ -63,8 +64,8 @@ const Background = ({
         <div className="absolute w-full h-full rounded-lg ring-1 ring-offset-0 ring-inset ring-white/10 z-50" />
         <Controls />
         <TitleBar>
-          <Metadata channel={channel} />
-          {/* <Rainwave initialData={rainwave} /> */}
+          <Metadata key="metadata" channel={channel} />
+          {hasMounted && <Rainwave key="rainwave" initialData={rainwave} />}
         </TitleBar>
         <div className="grid grid-cols-[92px_196px_1600px] h-full">
           <div className="flex flex-col h-full bg-gradient-to-b from-black/50 to-black/30 shadow-sidebar-inset rounded-l-lg">
