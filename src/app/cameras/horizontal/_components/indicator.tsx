deb09abffd57091edebd398013ce762cf604b796
Bryan Veloso <bryan@velo.so>
2023-12-03 12:20:19 -0800

    Working indicator to show mute status.

--- src/app/cameras/horizontal/_components/indicator.tsx
+++ src/app/cameras/horizontal/_components/indicator.tsx
@@ -1,9 +1,28 @@
 'use client';
 
+import { FC, useEffect, useState } from 'react';
+
+import { useSocket } from '@/hooks/use-socket';
 import { cn } from '@/lib/utils';
 
-export const Indicator = () => {
+export const Indicator: FC = () => {
+  const [isMuted, setIsMuted] = useState(false);
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    socket?.on('obs:microphone', ({ muted }) => {
+      setIsMuted(muted);
+    });
+  }, [socket]);
+
   return (
-    <div className="absolute right-6 top-12 z-50 h-[148px] w-1 rounded-l-sm bg-main-avagreen shadow-[1px_0_10px_#5be058]"></div>
+    <div
+      className={cn(
+        isMuted
+          ? 'bg-red-600 shadow-[2px_0_12px_theme(colors.red.600)]'
+          : 'bg-main-avagreen shadow-[2px_0_12px_theme(colors.main.avagreen)]',
+        'absolute right-6 top-12 z-50 h-[148px] w-1 rounded-l-sm transition-colors'
+      )}
+    ></div>
   );
 };
