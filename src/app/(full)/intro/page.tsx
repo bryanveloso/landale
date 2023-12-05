bab240c2a436400fd93b703c21e53f419955c55d
Bryan Veloso <bryan@velo.so>
2023-12-05 10:37:02 -0800

    SOON.

---
+++ src/app/(full)/intro/page.tsx
@@ -0,0 +1,14 @@
+const Page = () => {
+  return (
+    <>
+      <div className="flex flex-auto items-center p-64">
+        <div className="flex-auto rounded-lg border-b border-white/25 p-5 text-4xl font-extrabold text-white">
+          SOON™️
+        </div>
+      </div>
+      <div className="absolute left-0 top-0 -z-10 h-full w-full bg-[url(/xenoavas.png)] opacity-50 blur-sm" />
+    </>
+  );
+};
+
+export default Page;
