840910b0c6386ab709e984fa777197e3ea5c70f2
Bryan Veloso <bryan@velo.so>
2024-05-07 01:55:40 -0700

    Re-add Tailwind.

--- packages/overlays/src/main.tsx
+++ packages/overlays/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import './index.css'
-
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-//
 import { routeTree } from './routeTree.gen';
+
+import './index.css';
 
 const router = createRouter({ routeTree });
 
