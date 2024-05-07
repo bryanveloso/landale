c3d78748c2aea1a2eaa083fd61208aa1adaa9d80
Bryan Veloso <bryan@velo.so>
2024-05-07 01:09:31 -0700

    Stub in Tanstack's React router.

--- packages/overlays/src/main.tsx
+++ packages/overlays/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
 import './index.css'
+
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+
+//
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- packages/overlays/vite.config.ts
+++ packages/overlays/vite.config.ts
@@ -1,9 +1,10 @@
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   server: {
     host: true,
     port: 8088,
