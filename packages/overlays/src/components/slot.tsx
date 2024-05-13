b6368b715ada8c0b4e4cacd25f4553331598bf7d
Bryan Veloso <bryan@velo.so>
2024-05-13 09:17:54 -0700

    Welcome back gradients.

--- packages/overlays/src/components/slot.tsx
+++ packages/overlays/src/components/slot.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 export const Slot: FC<{ width: string; height: string }> = ({ width, height }) => (
-  <div className="bg-shark-800 flex rounded-2xl shadow-xl shadow-black/50">
+  <div className="from-shark-800 to-shark-900 flex rounded-2xl bg-gradient-to-b shadow-xl shadow-black/50 ring-2 ring-inset ring-white/10">
     <div className="bg-shark-950 relative m-3 flex rounded-lg p-1.5 shadow-[inset_0_0_0_1px_theme(colors.shark.950)]">
       <div className={`bg-avayellow ${width} ${height}`}></div>
     </div>
