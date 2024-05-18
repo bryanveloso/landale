bdbf8d117359369b8b0f28c46d7ac202f0b4b3d2
Bryan Veloso <bryan@velo.so>
2024-05-18 00:19:12 -0700

    Implement the new box styling.

--- packages/overlays/src/routes/(widget)/omnywidget.tsx
+++ packages/overlays/src/routes/(widget)/omnywidget.tsx
@@ -1,23 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import avalonstar from '~/avalonstar.png';
-
 export const Route = createFileRoute('/(widget)/omnywidget')({
   component: Omnywidget,
 });
 
 function Omnywidget() {
   return (
-    <div className="w-canvas h-canvas relative flex flex-col justify-between">
+    <div className="relative flex h-canvas w-canvas flex-col justify-between">
       {/* Widget */}
       <div className="flex justify-end">
         <div className="m-6 flex flex-col">
-          <div className="bg-shark-800 flex rounded-2xl ring-2 shadow-xl shadow-black/50 ring-white/10 ring-inset">
-            <div className="bg-shark-950 relative m-3 rounded-lg p-1.5 shadow-[inset_0_0_0_1px_theme(colors.shark.950)]">
+          <div className="flex rounded-2xl bg-shark-800 bg-gradient-to-b from-white/20 to-black/20 bg-blend-soft-light shadow-xl shadow-black/50 ring-2 ring-inset ring-white/10">
+            <div className="relative m-3 rounded-lg bg-shark-950 p-1.5 shadow-[inset_0_0_0_1px_theme(colors.shark.950)]">
               <div className="flex w-96 items-center gap-1">
-                <div className="bg-muted-bluegrey h-12 flex-auto grow rounded ring-2 ring-white/30 ring-inset"></div>
+                <div className="h-12 flex-auto grow rounded bg-shark-950 bg-gradient-to-b from-white/20 to-black/20 bg-blend-soft-light ring-2 ring-inset ring-white/10"></div>
                 <div className="m-2 w-8">
-                  <img src={avalonstar} />
+                  <img src="./avalonstar.png" />
                 </div>
               </div>
             </div>
