09a84b62d8e902b8c2ee3fac241a4fc9d5b9345d
Bryan Veloso <bryan@velo.so>
2024-12-18 23:09:39 -0800

    Add a basic LiveSplit container.

---
+++ packages/overlays/src/routes/(full)/speedrunning.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+import { Slot } from '@/components/slot';
+
+export const Route = createFileRoute('/(full)/speedrunning')({
+  component: Speedrunning,
+});
+
+function Speedrunning() {
+  return (
+    <div className="relative flex h-canvas w-canvas flex-col justify-between">
+      {/* Sidebar */}
+      <aside className="flex justify-end">
+        <div className="m-6 flex flex-col gap-3">
+          <Slot width="w-96">
+            <div className="h-[636px]" />
+          </Slot>
+        </div>
+      </aside>
+    </div>
+  );
+}
