c91e097844edd42168094ab5f4b3d30ca5a610dd
Bryan Veloso <bryan@velo.so>
2025-04-05 14:57:02 -0700

    Fix OBSProvider not actually connecting.

--- packages/overlays/src/lib/obs.provider.tsx
+++ packages/overlays/src/lib/obs.provider.tsx
@@ -1,29 +1,27 @@
 import OBSWebSocket from 'obs-websocket-js'
-import { type FC, type PropsWithChildren, createContext, useEffect, useMemo } from 'react'
+import { type FC, type PropsWithChildren, createContext, useEffect } from 'react'
 
 export const OBSContext = createContext(null)
 
-const socketUrl = 'ws://demi.local:4455'
+const socketUrl = 'ws://demi:4455'
 
 export const OBSProvider: FC<PropsWithChildren> = ({ children }) => {
-  const obs = useMemo(() => new OBSWebSocket(), [])
-
   useEffect(() => {
+    const obs = new OBSWebSocket()
     const init = async () => {
       try {
-        const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(socketUrl, 'pVH9gCpaOQniUW6i', {
-          rpcVersion: 1
-        })
+        const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(socketUrl, 'yfX1E3UyKP3gTQ2e')
         console.log(
           `🟢 <OBSContext /> connected. obs-websocket-js: ${obsWebSocketVersion}, rpc: ${negotiatedRpcVersion}`
         )
-      } catch (error) {
-        console.error(`🔴 <OBSContext /> failed to connect:`, error)
+      } catch (err: unknown) {
+        const error = err as Error
+        console.error(`🔴 <OBSContext /> failed to connect:`, error.message)
       }
     }
 
     init()
-  }, [obs])
+  }, [])
 
   return <OBSContext.Provider value={null}>{children}</OBSContext.Provider>
 }
