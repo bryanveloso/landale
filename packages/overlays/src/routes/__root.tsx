b10ce2b8625d651ec6576fcce51aec61d10ed08d
Bryan Veloso <bryan@velo.so>
2025-04-08 00:20:02 -0700

    Get rid of the devtools warning.

--- packages/overlays/src/routes/__root.tsx
+++ packages/overlays/src/routes/__root.tsx
@@ -28,7 +28,7 @@ const TanStackRouterDevtools =
   'obsstudio' in window
     ? () => null
     : React.lazy(() =>
-        import('@tanstack/router-devtools').then((res) => ({
+        import('@tanstack/react-router-devtools').then((res) => ({
           default: res.TanStackRouterDevtools
         }))
       )
