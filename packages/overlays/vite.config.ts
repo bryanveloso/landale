81f82aa8f04c36664110291c7364544c3ad9b762
Bryan Veloso <bryan@velo.so>
2025-05-26 15:34:32 -0700

    Replace @tailwindcss/postcss with @tailwindcss/vite.

--- packages/overlays/vite.config.ts
+++ packages/overlays/vite.config.ts
@@ -1,11 +1,12 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
+  plugins: [react(), TanStackRouterVite(), tsconfigPaths(), tailwindcss()],
   server: {
     allowedHosts: ['saya.local'],
     host: true,
