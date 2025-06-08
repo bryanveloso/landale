f163d13c053e969004b60a4935516edafaceccda
Bryan Veloso <bryan@velo.so>
2025-06-08 00:28:50 -0700

    Guess we're done with the debug toolbars.

--- packages/overlays/src/routes/__root.tsx
+++ packages/overlays/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, type PropsWithChildren, Suspense } from 'react'
+import { type FC, type PropsWithChildren } from 'react'
 import { QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 
@@ -15,34 +15,8 @@ export const Route = createRootRouteWithContext<{
   )
 })
 
-// Lazy load the devtools if running outside of OBS Studio.
-const ReactQueryDevtools =
-  'obsstudio' in window
-    ? () => null
-    : React.lazy(() =>
-        import('@tanstack/react-query-devtools').then((res) => ({
-          default: res.ReactQueryDevtools
-        }))
-      )
-const TanStackRouterDevtools =
-  'obsstudio' in window
-    ? () => null
-    : React.lazy(() =>
-        import('@tanstack/react-router-devtools').then((res) => ({
-          default: res.TanStackRouterDevtools
-        }))
-      )
-
 const RootDocument: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <>
-      {children}
-      {/* <Suspense>
-        <ReactQueryDevtools />
-        <TanStackRouterDevtools />
-      </Suspense> */}
-    </>
-  )
+  return <>{children}</>
 }
 
 function RootComponent() {
