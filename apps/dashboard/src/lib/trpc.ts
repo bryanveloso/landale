c25f383802ffe0ba0b057c55105ce68dab971e4a
Bryan Veloso <bryan@velo.so>
2025-06-29 09:13:55 -0700

    Have dashboard and overlays match configs a little.

--- apps/dashboard/src/lib/trpc.ts
+++
@@ -1,7 +0,0 @@
-import { trpcClient } from '@/lib/trpc-client'
-import type { inferRouterOutputs } from '@trpc/server'
-import type { AppRouter } from '@landale/server'
-
-export type RouterOutputs = inferRouterOutputs<AppRouter>
-
-export { trpcClient as trpc }
