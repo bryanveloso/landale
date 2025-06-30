9b32d62f5bbff18c718f706bf20935fa8dbf422d
Bryan Veloso <bryan@velo.so>
2025-06-30 10:51:59 -0700

    Fix DB testing configuration.

--- apps/server/config/test.exs
+++ apps/server/config/test.exs
@@ -5,13 +5,21 @@ import Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :server, Server.Repo,
-  username: "Avalonstar",
-  password: "",
-  hostname: "localhost",
-  database: "server_test#{System.get_env("MIX_TEST_PARTITION")}",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
+# Use DATABASE_URL if provided (CI), otherwise use local config
+if database_url = System.get_env("DATABASE_URL") do
+  config :server, Server.Repo,
+    url: database_url,
+    pool: Ecto.Adapters.SQL.Sandbox,
+    pool_size: System.schedulers_online() * 2
+else
+  config :server, Server.Repo,
+    username: System.get_env("USER"),
+    password: "",
+    hostname: "localhost",
+    database: "server_test#{System.get_env("MIX_TEST_PARTITION")}",
+    pool: Ecto.Adapters.SQL.Sandbox,
+    pool_size: System.schedulers_online() * 2
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
