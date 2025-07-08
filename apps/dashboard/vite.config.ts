42ad2f7e429cc4965f50bf17d20994e01bb2a9d9
Bryan Veloso <bryan@velo.so>
2025-07-08 11:53:08 -0700

    No big deal, just throwing a Tauri app into the mix.

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -4,9 +4,31 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [tsconfigPaths(), tanstackRouter({ target: 'solid', autoCodeSplitting: true }), solid(), tailwindcss()],
+const host = process.env.TAURI_DEV_HOST || 'localhost'
+
+export default defineConfig(async () => ({
+  plugins: [
+    tsconfigPaths(), 
+    tanstackRouter({ target: 'solid', autoCodeSplitting: true }), 
+    solid(), 
+    tailwindcss()
+  ],
+  clearScreen: false,
   server: {
-    allowedHosts: ['zelan']
+    host: host || false,
+    port: 5174,
+    strictPort: true,
+    hmr: host ? {
+      protocol: 'ws',
+      host: host,
+      port: 5175
+    } : undefined,
+    allowedHosts: ['zelan', 'localhost']
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG
   }
-})
+}))
