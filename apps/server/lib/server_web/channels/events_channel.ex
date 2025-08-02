6c80eb99ad0d67be07b9d43c9def49664b176d40
Bryan Veloso <bryan@velo.so>
2025-08-02 16:26:27 -0700

    Fix EventsChannel crash on Twitch connection events.

--- apps/server/lib/server_web/channels/events_channel.ex
+++ apps/server/lib/server_web/channels/events_channel.ex
@@ -80,6 +80,28 @@ defmodule ServerWeb.EventsChannel do
   end
 
   @impl true
+  def handle_info({:twitch_connected, event}, socket) do
+    push(socket, "event", %{
+      type: "twitch_connected",
+      data: event,
+      timestamp: System.system_time(:second)
+    })
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:twitch_disconnected, event}, socket) do
+    push(socket, "event", %{
+      type: "twitch_disconnected",
+      data: event,
+      timestamp: System.system_time(:second)
+    })
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def handle_info({:twitch_connection_changed, event}, socket) do
     push(socket, "event", %{
       type: "twitch_connection",
