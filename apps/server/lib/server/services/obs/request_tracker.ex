5dc971ba25204855c12a519ee6d266f2401549e4
Bryan Veloso <bryan@velo.so>
2025-08-02 11:07:51 -0700

    Fix RequestTracker from parameter handling to prevent timeouts.

--- apps/server/lib/server/services/obs/request_tracker.ex
+++ apps/server/lib/server/services/obs/request_tracker.ex
@@ -33,7 +33,7 @@ defmodule Server.Services.OBS.RequestTracker do
   end
 
   @impl true
-  def handle_call({:send_request, request_type, request_data, from, ws_conn}, _from, state) do
+  def handle_call({:send_request, request_type, request_data, original_from, ws_conn}, from, state) do
     # Generate request ID
     request_id = to_string(state.next_id)
 
@@ -48,6 +48,7 @@ defmodule Server.Services.OBS.RequestTracker do
 
         request_info = %{
           from: from,
+          original_from: original_from,
           type: request_type,
           data: request_data,
           timer: timer_ref,
