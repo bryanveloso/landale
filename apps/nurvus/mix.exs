1192ca38510628727c2be22543cd2603479bada5
Bryan Veloso <bryan@velo.so>
2025-08-05 16:17:04 -0700

    Store Nurvus process logs in XDG-approved location.

--- apps/nurvus/mix.exs
+++ apps/nurvus/mix.exs
@@ -36,7 +36,7 @@ defmodule Nurvus.MixProject do
   defp releases do
     [
       nurvus: [
-        version: "2025.08.05c",
+        version: "2025.08.05d",
         applications: [nurvus: :permanent],
         include_executables_for: [:unix, :windows],
         steps: [:assemble, &Burrito.wrap/1],
